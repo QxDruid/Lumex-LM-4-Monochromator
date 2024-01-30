34ed8f03f38cf524e4097f2ef6bf0646d51d2c3f
xDruid <savelyev.eg@gmail.com>
2024-01-30 11:50:50 +0700

    add input validator

--- main.go
+++ main.go
@@ -84,13 +84,31 @@ func convert_state_to_wavelength(state string) string {
 	return wavelength_str
 }
 
+func input_wl_validator(input string) int {
+
+	input_float, err := strconv.ParseFloat(input, 8)
+	if err != nil {
+		return 0
+	}
+	input_int := int(input_float * 10)
+	if input_int > 9000 || input_int < 2200 {
+		return 0
+	}
+	fmt.Println()
+	return 1
+}
+
 func main() {
 	a := app.New()
 	window := a.NewWindow("test")
-	window.Resize(fyne.NewSize(320, 400))
+	window.Resize(fyne.NewSize(320, 450))
 
-	label_1 := widget.NewLabel("Current WL:")
-	label_1.Move(fyne.NewPos(80, 20))
+	label_status := widget.NewLabel("Is OK!")
+	label_status.Move(fyne.NewPos(40, 400))
+	label_status.Resize(fyne.NewSize(140, 40))
+
+	label_1 := widget.NewLabel("Current WL (nm):")
+	label_1.Move(fyne.NewPos(60, 20))
 	label_1.Resize(fyne.NewSize(140, 40))
 	label_current_wl := widget.NewLabel("400")
 	label_current_wl.Move(fyne.NewPos(180, 20))
@@ -106,12 +124,22 @@ func main() {
 
 	btn_go_to_wl := widget.NewButton("Go", func() {
 		data := entry_go_wl.Text
+		err_ := input_wl_validator(data)
+		if err_ == 0 {
+			label_status.SetText("False Wavelength")
+			return
+		}
 		wavelength_to_go, err := strconv.ParseFloat(data, 8)
 		if err != nil {
 			log.Fatal(err)
 		}
+		label_status.SetText("In Process")
 		wavelength_to_go *= 10
 		go_to(int(wavelength_to_go))
+		current_wl := read_state()
+		current_wl = convert_state_to_wavelength(data)
+		label_current_wl.SetText(current_wl)
+		label_status.SetText("Is Ok")
 
 	})
 	btn_go_to_wl.Move(fyne.NewPos(40, 120))
@@ -142,9 +170,23 @@ func main() {
 	entry_go_to_wl.Resize(fyne.NewSize(100, 40))
 
 	btn_go_from_to := widget.NewButton("Go", func() {
-		data := read_state()
-		data = convert_state_to_wavelength(data)
-		label_current_wl.SetText(data)
+		data_from := entry_go_from_wl.Text
+		err_ := input_wl_validator(data_from)
+		if err_ == 0 {
+			label_status.SetText("False Wavelength From Go")
+			return
+		}
+
+		data_to := entry_go_to_wl.Text
+		err_ = input_wl_validator(data_to)
+		if err_ == 0 {
+			label_status.SetText("False Wavelength To Go")
+			return
+		}
+		label_status.SetText("In Process")
+
+		//go_from_to(from_int, to_int)
+		label_status.SetText("Is Ok")
 	})
 
 	label_delay := widget.NewLabel("Step Delay")
@@ -159,7 +201,7 @@ func main() {
 	btn_go_from_to.Resize(fyne.NewSize(240, 40))
 
 	window_content := container.NewWithoutLayout(label_1, label_current_wl, label_go_wl, entry_go_wl, btn_go_to_wl, btn_read_state, label_go_from_wl,
-		entry_go_from_wl, label_go_to_wl, entry_go_to_wl, btn_go_from_to, label_delay, entry_delay)
+		entry_go_from_wl, label_go_to_wl, entry_go_to_wl, btn_go_from_to, label_delay, entry_delay, label_status)
 	window.SetContent(window_content)
 	window.ShowAndRun()
 }
